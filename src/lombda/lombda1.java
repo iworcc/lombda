58a855c2ec7544ae37c9efe53dc0edd1eb040daa
iwor <769737461@qq.com>
2018-08-16 22:21:36 +0800

    first commit

---
+++ src/lombda/lombda1.java
@@ -0,0 +1,18 @@
+package lombda;
+
+import java.io.Closeable;
+import java.util.function.Consumer;
+
+public class lombda1 {
+	
+	public static void main(String[] args) {
+		
+		
+		Runnable r = () -> {};
+		Closeable c =() -> {};
+		
+		Consumer<String> c1 = a -> {};
+		
+	}
+
+}
